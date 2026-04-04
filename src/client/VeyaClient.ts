230dcd2b8bdc5d309c2bd088cce050ed13614a8a
veyanet <tonyiron9225@gmail.com>
2026-04-04 05:34:33 +0000

    feat: implement config parsing and modules structure in client constructor

--- src/client/VeyaClient.ts
+++ src/client/VeyaClient.ts
@@ -1,5 +1,10 @@
-import { HttpClient } from './http.js';
-import { VeyaConfig } from '../config.js';
+import { HttpClient } from "./http.js";
+import { VeyaConfig } from "../config.js";
+import { EnvironmentsResource } from "../modules/environments.js";
 export class Veya {
-  constructor(config: VeyaConfig = {}) {}
+  public readonly environments: EnvironmentsResource;
+  constructor(config: VeyaConfig = {}) {
+    const http = new HttpClient(config);
+    this.environments = new EnvironmentsResource(http);
+  }
 }
