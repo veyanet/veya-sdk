3f485ae5ada79fede867df8d9644df4d83751fa9
veyanet <tonyiron9225@gmail.com>
2026-04-08 12:57:07 +0000

    feat: create auth module exports index file

---
+++ src/auth/index.ts
@@ -0,0 +1,2 @@
+export { walletAuth, type WalletAuthInput, type WalletSignMessage, type WalletAuthResult } from "./wallet.js";
+export { createClientConfigFromEnv, describeApiKeyTier, type ApiKeyTier } from "./apiKey.js";
