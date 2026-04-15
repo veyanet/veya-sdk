1b25202a84c86e33e7de889615a09da54e86018c
veyanet <tonyiron9225@gmail.com>
2026-04-15 14:06:12 +0000

    feat: finalize environments resource module implementation

--- src/modules/environments.ts
+++ src/modules/environments.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '../client/http.js';
+import { HttpClient } from "../client/http.js";
 export class EnvironmentsResource {
   constructor(private readonly http: HttpClient) {}
-  async list() { return []; }
+  async list() {
+    return this.http.request("/v1/environments");
+  }
 }
