49f18f4c7600908166be825963856fdacc677e34
veyanet <tonyiron9225@gmail.com>
2026-04-17 04:30:30 +0000

    feat: finalize agents resource module implementation

--- src/modules/agents.ts
+++ src/modules/agents.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '../client/http.js';
+import { HttpClient } from "../client/http.js";
 export class AgentsResource {
   constructor(private readonly http: HttpClient) {}
-  async list() { return []; }
+  async deploy(envId: string, options: any) {
+    return this.http.request(`/v1/environments/${envId}/agents`, { method: "POST", body: options });
+  }
 }
