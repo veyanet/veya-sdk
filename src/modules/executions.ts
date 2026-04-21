b82167ae2525ad1ea229b589043b2c0882df6b8c
veyanet <tonyiron9225@gmail.com>
2026-04-21 14:46:09 +0000

    feat: finalize executions resource module implementation

--- src/modules/executions.ts
+++ src/modules/executions.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '../client/http.js';
+import { HttpClient } from "../client/http.js";
 export class ExecutionsResource {
   constructor(private readonly http: HttpClient) {}
-  async list() { return []; }
+  async get(id: string) {
+    return this.http.request(`/v1/executions/${id}`);
+  }
 }
